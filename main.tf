b517a7bd38202196e2f3e993657e288349804a1e
moffzilla <jfrancisco.poo@gmail.com>
2021-07-08 16:13:40 -0500

    Attaching SG by ID

--- main.tf
+++ main.tf
@@ -35,12 +35,13 @@ data "template_file" "user_data" {
 resource "aws_instance" "web" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
+  availability_zone = var.region
 #  subnet_id                   = aws_subnet.subnet_public.id
 #  vpc_security_group_ids      = [aws_security_group.sg_22_80.id]
   associate_public_ip_address = true
   user_data                   = data.template_file.user_data.rendered
   security_groups = [
-      "web-in"
+      "sg-24b4946e"
   ]  
 
   tags = {
