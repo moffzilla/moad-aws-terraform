fc23fe4c7b0313a91cb4f97d2a20220ac0c3fed2
moffzilla <jfrancisco.poo@gmail.com>
2021-07-08 12:12:45 -0500

    VPC Removed for testing

--- main.tf
+++ main.tf
@@ -28,70 +28,6 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-#resource "aws_vpc" "vpc" {
-#  cidr_block           = var.cidr_vpc
-#  enable_dns_support   = true
-#  enable_dns_hostnames = true
-#}
-
-#resource "aws_internet_gateway" "igw" {
-#  vpc_id = aws_vpc.vpc.id
-#}
-
-#resource "aws_subnet" "subnet_public" {
-#  vpc_id     = aws_vpc.vpc.id
-#  cidr_block = var.cidr_subnet
-#}
-
-#resource "aws_route_table" "rtb_public" {
-#  vpc_id = aws_vpc.vpc.id
-
-#  route {
-#    cidr_block = "0.0.0.0/0"
-#    gateway_id = aws_internet_gateway.igw.id
-#  }
-#}
-
-#resource "aws_route_table_association" "rta_subnet_public" {
-#  subnet_id      = aws_subnet.subnet_public.id
-#  route_table_id = aws_route_table.rtb_public.id
-#}
-
-resource "aws_security_group" "sg_22_80" {
-  name   = "sg_22"
-  vpc_id = vpc-0b8826c3fc8e9452e
-
-  # SSH access from the VPC
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-
-  ingress {
-    from_port   = 8080
-    to_port     = 8080
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 data "template_file" "user_data" {
   template = file("scripts/add-ssh-web-app.yaml")
 }
@@ -100,7 +36,7 @@ resource "aws_instance" "web" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
 #  subnet_id                   = aws_subnet.subnet_public.id
-  vpc_security_group_ids      = [aws_security_group.sg_22_80.id]
+#  vpc_security_group_ids      = [aws_security_group.sg_22_80.id]
   associate_public_ip_address = true
   user_data                   = data.template_file.user_data.rendered
 
