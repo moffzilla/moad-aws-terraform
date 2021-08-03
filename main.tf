6f87fd41bc109b4cdf4a66b7f4d95e2d197aeaf3
moffzilla <jfrancisco.poo@gmail.com>
2021-08-03 17:41:06 -0500

    goapp-v2

--- main.tf
+++ main.tf
@@ -28,9 +28,38 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+resource "aws_vpc" "vpc" {
+  cidr_block           = var.cidr_vpc
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+}
+
+resource "aws_subnet" "subnet_public" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = var.cidr_subnet
+}
+
+resource "aws_route_table" "rtb_public" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta_subnet_public" {
+  subnet_id      = aws_subnet.subnet_public.id
+  route_table_id = aws_route_table.rtb_public.id
+}
+
 resource "aws_security_group" "sg_22_80" {
   name   = "sg_22"
-  vpc_id =  "vpc-e867aa92"
+  vpc_id = aws_vpc.vpc.id
 
   # SSH access from the VPC
   ingress {
@@ -63,38 +92,27 @@ resource "aws_security_group" "sg_22_80" {
   }
 }
 
-
-data "aws_subnet_ids" "example" {
-  vpc_id = "vpc-e867aa92"
-}
-
-data "aws_subnet" "example" {
-  for_each = data.aws_subnet_ids.example.ids
-  id       = each.value
-}
-
 data "template_file" "user_data" {
   template = file("scripts/add-ssh-web-app.yaml")
-}
-
-resource "aws_instance" "web" {
-  for_each      = data.aws_subnet_ids.example.ids
-  ami                         = data.aws_ami.ubuntu.id
-  instance_type               = "t2.micro"
-  #availability_zone = var.region
-  subnet_id                   = each.value
-  vpc_security_group_ids      = [data.aws_security_group.selected.vpc_id]
-  associate_public_ip_address = true
-  user_data                   = data.template_file.user_data.rendered
-#  security_groups = [
-#      "sg-24b4946e"
-#  ]  
-
-  tags = {
-    "cost_center" = var.cost_center
+  vars = {
+    ssh_public_key     = var.ssh_credentials
   }
 }
 
-#output "public_ip" {
-#  value = aws_instance.web["${each.key}"].public_ip
-#}
+resource "aws_instance" "web" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.subnet_public.id
+  vpc_security_group_ids      = [aws_security_group.sg_22_80.id]
+  associate_public_ip_address = true
+  user_data                   = data.template_file.user_data.rendered
+
+  tags = {
+    Name = "MOAD Front-End",
+    Cost_Center = var.cost_center
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.web.public_ip
+}
